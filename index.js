af1aa861a418120d74cfd51f1982f03a277959db
NatalieBM1 <127702162+NatalieBM1@users.noreply.github.com>
2025-05-17 02:55:01 -0500

    Habilitar CORS

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser =require('body-parser');
-const PaisesRoutes =require('./routes/PaisesRoutes');
-const GobiernoRoutes =require('./routes/GobiernosRoutes');
-const LideresPoliticosRoutes =require('./routes/LideresPoliticosRoutes');
+const bodyParser = require('body-parser');
+const cors = require('cors');  
+const PaisesRoutes = require('./routes/PaisesRoutes');
+const GobiernoRoutes = require('./routes/GobiernosRoutes');
+const LideresPoliticosRoutes = require('./routes/LideresPoliticosRoutes');
 const morgan = require('morgan');
-
 
 const app = express();
 
-// conexion a mongo db 
+// Conexion a mongo db 
+mongoose.connect('mongodb+srv://admin:admin@pdb.rpwsgda.mongodb.net/NationPolitics')
+    .then(() => console.log('Conectado a Mi base de datos Mongo Db'))
+    .catch(err => console.error("Error al conectar a mi DB", err));
 
-mongoose.connect('mongodb+srv://admin:admin@pdb.rpwsgda.mongodb.net/NationPolitics',
-    ).then(()=> console.log('Conectado a Mi base de datos Mongo Db'))
-.catch (err=> console.error("Error al conectar a mi DB",err));
+
+app.use(cors());
 
 app.use(bodyParser.json());
-app.use(PaisesRoutes);
-app.use('/gob',GobiernoRoutes);
-app.use('/lideres',LideresPoliticosRoutes);
-app.use(morgan('dev'));	
-app.listen(3005,()=>{
-    console.log("Server ON - Port 3005")
-})
+app.use('/paises',PaisesRoutes);
+app.use('/gobiernos', GobiernoRoutes);
+app.use('/lideres', LideresPoliticosRoutes);
+app.use(morgan('dev'));
+
+app.listen(3005, () => {
+    console.log("Server ON - Port 3005");
+});
